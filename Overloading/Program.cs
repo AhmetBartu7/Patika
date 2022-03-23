6f60b34abe475412b5987afc32c1b9054eb8c9ac
Ahmet Bartu <96727284+AhmetBartu7@users.noreply.github.com>
2022-03-23 12:53:17 +0300

    Add files via upload

---
+++ Overloading/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // OUT Parametreler
+            string sayi ="999";
+            int outSayi;
+
+            bool sonuc= int.TryParse(sayi, out  outSayi);
+            if(sonuc)
+            {
+                System.Console.WriteLine("Basarili");
+                System.Console.WriteLine( outSayi);
+            }
+            else
+            {
+                System.Console.WriteLine("Basarisiz");
+            }
+
+            Metotlar instance = new Metotlar();
+            instance.Topla(4,5, out int toplamSonucu);
+            System.Console.WriteLine(toplamSonucu);
+
+            //Metot Aşırı Yükleme = Overloading
+
+            int ifade = 777;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("BB","7");
+
+            // Metot İmzası
+            // MetotAdı + parametre sayisi + parametre
+        }
+    }
+
+    class Metotlar
+    {
+        public void Topla(int a, int b, out int toplam)
+        {
+            toplam = a+b;
+
+        }
+
+        public void EkranaYazdir(string veri)
+        {
+            System.Console.WriteLine( veri);
+        }
+
+          public void EkranaYazdir(int veri)
+        {
+            System.Console.WriteLine( veri);
+        }
+
+          public void EkranaYazdir(string veri1, string veri2)
+        {
+            System.Console.WriteLine( veri1 + veri2);
+        }
+    }
+}
---
+++ Resurcive_Extension/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static async void Main(string[] args)
+        {
+            
+
+
+            // Rekürsif - Öz Yineleme
+            
+
+            int result =1;
+            for (int i=1; i<5; i++)
+            result = result =3;
+            System.Console.WriteLine(result);
+
+            Islemler instance = new();
+            System.Console.WriteLine(instance.Expo(3,4));
+
+        
+            //Extension Metotlar
+        string ifade = "BB7";
+        bool sonuc = ifade.CheckSpaces();
+        System.Console.WriteLine(sonuc);
+        if(sonuc)
+        
+        System.Console.WriteLine(ifade.RemoveWhiteSpaces());
+        System.Console.WriteLine(ifade.MakeUpperCase());
+        System.Console.WriteLine(ifade.MakeLowerCase());
+
+        int[] dizi = {9,3,6,2,5,0};
+        
+        dizi.SortArray();
+        dizi.EkranaYazdir();
+
+        int sayi =5;
+        System.Console.WriteLine(sayi.IsEvenNumber());  
+        System.Console.WriteLine(ifade.GetFirstCharacter());   
+
+
+
+
+        }
+    }
+
+    public class Islemler
+{
+    public int Expo (int sayi, int üs)
+    {
+        if(üs<2)
+        return sayi;
+        return Expo(sayi, üs-1)*sayi;
+    }
+
+    //Expo(3,4)
+    //Expo(3,3)*3;
+    //Expo(3,2)*3*3;
+    //Expo(3-1)*3*3*3;
+    // 3*3*3*3 = 3*4;
+
+
+}
+
+public static class Extension
+
+{
+    public static bool CheckSpaces(this string param)
+    {
+        return param.Contains("");
+    } 
+
+    public static string RemoveWhiteSpaces(this string param)
+    {
+        string[] dizi = param.Split("");
+        return string.Join("", dizi);
+
+    }
+
+     public static string MakeUpperCase(this string param)
+    {
+        return param.ToUpper();
+    } 
+    public static string MakeLowerCase(this string param)
+    {
+        return param.ToLower();
+    }
+    public static int[] SortArray(this int[] param)
+    {
+        Array.Sort(param);
+        return param;
+    }
+    public static void EkranaYazdir(this int[] param)
+    {
+        foreach(int item in param)
+        System.Console.WriteLine(item);
+    }
+
+    public static bool IsEvenNumber(this int param)
+    {
+        return param%2 ==0;
+    }
+
+    public static string GetFirstCharacter(this string param)
+    {
+        return param.Substring(0,1);
+    }
+
+}
+
+
+
+}
