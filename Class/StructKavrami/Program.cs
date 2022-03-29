6805a99348d1f47e1f45c0531b848b6c673e5789
Ahmet Bartu <96727284+AhmetBartu7@users.noreply.github.com>
2022-03-29 23:57:18 +0300

    Add files via upload

---
+++ Class/StructKavrami/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen();
+            dikdortgen.KisaKenar = 3;
+            dikdortgen.UzunKenar = 4;
+
+            System.Console.WriteLine("Class Alan Hesabi :{0}", dikdortgen.AlanHesapla());
+            Dikdortgen_Struct dikdortgen_Struct = new Dikdortgen_Struct();
+            dikdortgen_Struct.KisaKenar=3;
+            dikdortgen_Struct.UzunKenar=4;  
+
+            System.Console.WriteLine("Class Alan Hesabi :{0}", dikdortgen_Struct.AlanHesapla());
+            
+        }
+    }
+
+    class Dikdortgen
+    {
+            public int KisaKenar;
+            public int UzunKenar;
+            public long AlanHesapla()
+            {
+                return this.KisaKenar = this.UzunKenar;
+            }
+    }
+
+    struct Dikdortgen_Struct
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+        public long AlanHesapla()
+        {
+            return this.KisaKenar = this.UzunKenar;
+        }
+    }
+}
